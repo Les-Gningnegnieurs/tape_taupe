3b76cda8b0a793b6db9fd1ac3d394fee330039ae
JersonAngel <114172312+JersonAngel@users.noreply.github.com>
2022-11-15 03:54:56 -0500

    modification pour inclure detection de coup sur les taupes

--- src/tape_taupe.cpp
+++ src/tape_taupe.cpp
@@ -46,44 +46,67 @@ int actionneur_taupes()
     unsigned long temps_actuel = millis() ;
     unsigned long temps_dactivation = 2000;
 
-    if (taupe_choisie==0)
+    if (taupe_choisie == 0)
     {
         //activation de la taupe tant que la duree de temps maximale ne soit pas atteinte
         while (temps_actuel - temps_anterieur > temps_dactivation)
         {
             temps_anterieur = temps_actuel ;
             digitalWrite(Taupe_1_pin, HIGH);
-            if ()
-            //*** a faire peut-etre ailleurs: inclure la detection de la switch sur la taupe et ajouter un compteur (taupe_touchee) de points
 
+            /*On verifie si la switch est activee (donc si la valeur vu par la pine INPUT = celle donnee par la OUTPUt). */
+            if (digitalRead(inpin_switch_taupe_1) == HIGH)
+            {
+                coup_reussi = true;
+            }
         }
+        /* On desactive la taupe choisie, et la fonction termine en returnant la valeur de coup_reussi (true ou false)*/
+        digitalWrite(Taupe_1_pin, LOW);
+        return coup_reussi;
     }
-    else if (taupe_choisie==1)
+/***********Les etapes sont les memes pour les prochaines boucles "else if"  **************/
+    else if (taupe_choisie == 1)
     {
-        //activation de la taupe tant que la duree de temps maximale ne soit pas atteinte
+       
         while (temps_actuel - temps_anterieur > temps_dactivation)
         {
             temps_anterieur = temps_actuel ;
             digitalWrite(Taupe_2_pin, HIGH);
+            if (digitalRead(inpin_switch_taupe_2) == HIGH)
+            {
+                coup_reussi = true;
+            }
         }
-        
+        digitalWrite(Taupe_2_pin, LOW);
+        return coup_reussi;
     }
-    else if (taupe_choisie==2)
+    else if (taupe_choisie == 2)
     {
         while (temps_actuel - temps_anterieur > temps_dactivation)
         {
             temps_anterieur = temps_actuel ;
             digitalWrite(Taupe_3_pin, HIGH);
+            if (digitalRead(inpin_switch_taupe_3) == HIGH)
+            {
+                coup_reussi = true;
+            }
         }
+        digitalWrite(Taupe_3_pin, LOW);
+        return coup_reussi;
     }
-    else if (taupe_choisie==3)
+    else if (taupe_choisie == 3)
     {
         while (temps_actuel - temps_anterieur > temps_dactivation)
         {
             temps_anterieur = temps_actuel ;
             digitalWrite(Taupe_4_pin, HIGH);
+            if (digitalRead(inpin_switch_taupe_4) == HIGH)
+            {
+                coup_reussi = true;
+            }
         }
-        
+        digitalWrite(Taupe_4_pin, LOW);
+        return coup_reussi;
     }
 
     return coup_reussi;
