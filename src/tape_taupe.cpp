07df5cfc69b694c2379e20e7c4ebd58a354e26f4
JersonAngel <114172312+JersonAngel@users.noreply.github.com>
2022-11-14 21:43:19 -0500

    mise à jour fichiers

---
+++ src/tape_taupe.cpp
@@ -0,0 +1,10 @@
+#include "tape_taupe.h"
+#define RAND_MAX 3
+
+int Random()
+{
+    int valeur_taupe = random();
+
+    printf("valeur random = %d ", valeur_taupe);
+    return valeur_taupe;
+}
---
+++ src/tape_taupe.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+
+int Random();
